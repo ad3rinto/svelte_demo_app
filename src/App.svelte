a4ff13784dda643e79dcc5276fe663b64af88308
ad3rinto <aad3rinto@gmail.com>
2022-09-26 18:59:36 +0100

    add initial files

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	// import svelteLogo from './assets/svelte.svg'
+	// import Counter from './lib/Counter.svelte'
+	const name = "Adeniyi";
+</script>
+
+<main>
+	<h1>Hello {name}</h1>
+</main>
+
+<style>
+	.logo {
+		height: 6em;
+		padding: 1.5em;
+		will-change: filter;
+	}
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+	.logo.svelte:hover {
+		filter: drop-shadow(0 0 2em #ff3e00aa);
+	}
+	.read-the-docs {
+		color: #888;
+	}
+</style>
